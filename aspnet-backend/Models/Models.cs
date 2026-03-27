7b53cb7842c1b0eca1ed47d0ca3cdd240941511e
JustNanh <ah250905@gmail.com>
2026-03-27 08:47:31 +0700

    Cập nhật code mới

--- aspnet-backend/Models/Models.cs
+++ aspnet-backend/Models/Models.cs
@@ -23,7 +23,7 @@ public class Customer
 
 public class Category
 {
-    [Key] public int CategoryId { get; set; }
+    [Key] public int CategoryId { get; set; } 
     [Required, MaxLength(100)] public string CategoryName { get; set; } = "";
     public ICollection<Product> Products { get; set; } = new List<Product>();
 }
